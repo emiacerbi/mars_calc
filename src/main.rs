8009692d3c1cef7fc1dc96d2d7a479b18a1b81f6
Emiliano Acerbi <eacerbip@gmail.com>
2023-05-02 19:09:26 -0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
 
-    calculate_weight_on_mars(100.00);
+fn main() {
+    println!("Enter your weight (kg): ");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    50.0
+    (weight / 9.81) * 3.711
 }
